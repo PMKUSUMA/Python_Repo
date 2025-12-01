a656f96ac85c6e835b8387a750524c6ccaaf4dbf
PMKUSUMA <kusuma201202@gmail.com>
2025-12-01 19:27:28 +0530

    first semester

---
+++ file_1_.py
@@ -0,0 +1,6 @@
+#Tuple unpacking
+tuple_variable=(1,2,3, "a","b","c")
+a,b,c,*_=tuple_variable
+print(a)
+print(b)
+print(c)
---
+++ main.py
@@ -0,0 +1,8 @@
+# Tuple unpacking
+"""Tuple unpacking"""
+tuple_variable=(1, 2, 3, "a", "b", "c")
+a,b,c,*_=tuple_variable
+print(a)
+print(b)
+print(c)
+
